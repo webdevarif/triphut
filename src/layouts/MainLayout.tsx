dac97e523ee5e0d92fd3b1462a1c3175fe5a6008
Arif Hossin <46327525+webdevarif@users.noreply.github.com>
2023-03-10 23:49:21 +0600

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,17 +2,19 @@ import React, { ReactNode } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-interface Props {
+interface MainLayoutProps {
   children: ReactNode;
   newsletter?: boolean;
 }
 
-const MainLayout = ({ children, newsletter = true }: Props) => (
-  <>
-    <Header />
-    {children}
-    <Footer newsletter={newsletter} />
-  </>
-);
+const MainLayout: React.FC<MainLayoutProps> = ({ children, newsletter = true }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer newsletter={newsletter} />
+    </>
+  );
+};
 
 export default MainLayout;
